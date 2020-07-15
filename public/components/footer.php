0ad88aa2b619232cb0c3f1b01530a6ce663347df
webnetkz <8gg001@gmail.com>
2020-07-15 19:09:34 +0600

    update

--- public/components/footer.php
+++ public/components/footer.php
@@ -1,3 +1,23 @@
 ﻿<footer>
-    
+
 </footer>
+<script>
+
+    window.addEventListener('scroll', () => {
+        if(pageYOffset > 50) {
+            document.querySelector('header').style.backgroundColor = 'rgb(12, 77, 162)';
+            let menuItem = document.querySelectorAll('.menuItem');
+            for(let i = 0; i < menuItem.length; i++) {
+                menuItem[i].style.color = 'rgb(255, 255, 255)';
+            }
+            document.querySelector('.logoLink').style.backgroundColor = 'rgb(255, 255, 255)';
+        } else {
+            document.querySelector('header').style.backgroundColor = 'rgb(255, 255, 255)';
+            let menuItem = document.querySelectorAll('.menuItem');
+            for(let i = 0; i < menuItem.length; i++) {
+                menuItem[i].style.color = 'rgb(12, 77, 162)';
+            }
+            document.querySelector('.logoLink').style.backgroundColor = 'rgb(255, 255, 255)';
+        }
+    });
+</script>
--- public/components/searchBar.php
+++ public/components/searchBar.php
@@ -1,3 +1,8 @@
 ﻿<section id="searchBar">
-    
+    <form action="#" method="GET" id="searchForm">
+        <input type="text" class="searchInp" placeholder="Класс">
+        <input type="text" class="searchInp" placeholder="Предмет">
+        <input type="text" class="searchInp" placeholder="Пакет">
+        <input type="text" class="searchInp" placeholder="Язык">
+    </form>
 </section>
