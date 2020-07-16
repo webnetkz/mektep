55e3f1b1c3aa33810531ea1d2ce2a17d2137643b
webnetkz <8gg001@gmail.com>
2020-07-16 10:35:24 +0600

    change contacts

--- public/components/contacts.php
+++ public/components/contacts.php
@@ -1,28 +1,40 @@
 ﻿<section id="contacts">
-    <div class="logoDiv">
-        <img src="public/img/logoRu.webp" alt="Логотип издетельства Мектеп" class="logoContacts">
+    <div class="contacts">
+        <div>
+            <h3 class="contactHeader">Контакты</h3>
+            <hr>
+            <p class="contactP">
+                    <img src="public/img/icons/phone.png" alt="mektep" class="contactIcon">
+                +7 (727) 394 42 34 <br>
+                +7 (727) 229 15 72 <br>
+                <br>
+                    <img src="public/img/icons/fax.png" alt="mektep" class="contactIcon">
+                +7 (727) 394 37 58 <br>
+                +7 (727) 394 42 30 <br>
+                <br>
+                    <img src="public/img/icons/mail.png" alt="mektep" class="contactIcon">
+                mektep@mail.ru <br>
+                <br>
+                    <img src="public/img/icons/adress.png" alt="mektep" class="contactIcon">
+                г.Алматы, пр. Абая 143 (угол ул. Гагарина); <br>
+                г.Алматы, пр. Абая 150/230 ЖКК "Гаухартас", подъезд №7, блок 9, офис 856;
+            </p>
+        </div> 
+        <div class="logoDiv">
+            <img src="public/img/logoRu.webp" alt="Логотип издетельства Мектеп" class="logoContacts">
+        </div>
+        <div>
+            <h3 class="contactHeader">Реквизиты</h3>
+            <hr>
+            <p class="contactP">
+                РНН 600700021972 <br>
+                БИН 040240006726 <br>
+                ИИК KZ676010131000003881 <br>
+                БИК HSBKKZKX <br>
+                АО “Народный банк Казахстана”
+            </p>
+        </div>
     </div>
-    <pre>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </pre>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2906.578796110535!2d76.89102991499135!3d43.23928998711531!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x388369164473abb1%3A0x3c813944940e0cc0!2z0L_RgNC-0YHQvy4g0JDQsdCw0Y8gMTQzLCDQkNC70LzQsNGC0YsgMDUwMDAw!5e0!3m2!1sru!2skz!4v1594870891266!5m2!1sru!2skz" width="100%" height="450" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" class="gMap"></iframe>
 </section>
